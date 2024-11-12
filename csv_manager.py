e1604b3b644185e82135a66c06718c816f4431b3
PabloPatin <pashka.lop@mail.ru>
2024-11-12 19:41:25 +0300

    csv_manager added

---
+++ csv_manager.py
@@ -0,0 +1,71 @@
+import pprint
+from datetime import datetime
+from typing import NamedTuple
+
+import pandas as pd
+
+Weekday = str
+
+
+class Lesson(NamedTuple):
+    week_day: str
+    time: str
+    name: str
+
+
+class CSVScheduleLoader:
+    def __init__(self, filepath: str):
+        self.fp = filepath
+        self.df = self.read_schedule_file()
+
+    @staticmethod
+    def parse_time_interval(time_str):
+        start_time, end_time = time_str.split('-')
+        start_datetime = datetime.strptime(start_time, '%H.%M')
+        end_datetime = datetime.strptime(end_time, '%H.%M')
+        return start_datetime, end_datetime
+
+    @staticmethod
+    def make_time_interval(begin_time: datetime, end_time: datetime):
+        return datetime.strftime(begin_time, '%H.%M')+'-'+datetime.strftime(end_time, '%H.%M')
+
+    def read_schedule_file(self):
+        df = pd.read_csv(self.fp)
+        df['Время начала'], df['Время окончания'] = zip(*df['Время'].apply(self.parse_time_interval))
+        return df
+
+    def get_daily_schedule(self, group: str, week_day: str) -> list[Lesson]:
+        """Возвращает список занятий группы по заданноиу дню недели"""
+        daily_lessons = []
+
+        # Фильтруем по группе и дню недели и сортируем по времени
+        filtered_df = self.df[(self.df['День недели'] == week_day) & (self.df['Группа'] == group)].sort_values(
+            by='Время начала')
+
+        for _, row in filtered_df.iterrows():
+            lesson = Lesson(
+                week_day=row['День недели'],
+                time=self.make_time_interval(row['Время начала'], row['Время окончания']),
+                name=row['Занятие']
+            )
+            # Добавляем занятие в список для соответствующего дня недели
+            daily_lessons.append(lesson)
+        return daily_lessons
+
+    def get_weekly_schedule(self, group) -> dict[Weekday, list[Lesson]]:
+        """Возвращает словарь вида [День недели: Список занятий] для заданной группы"""
+        weekly_lessons = {}
+        for week_day in ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']:
+            weekly_lessons[week_day] = self.get_daily_schedule(group, week_day)
+        return weekly_lessons
+
+
+if __name__ == '__main__':
+    loader = CSVScheduleLoader(filepath='schedule.csv')
+
+    daily_lessons = loader.get_daily_schedule(group='АГС-22-1', week_day='Понедельник')
+    pprint.pp(daily_lessons)
+    print()
+
+    weekly_lessons = loader.get_weekly_schedule(group='АГС-22-1')
+    pprint.pp(weekly_lessons)
--- parse_timetables.py
+++ parse_timetables.py
@@ -3,23 +3,28 @@ import os
 from timetable_parser import TimetablePDFParser
 
 TIMETABLE_DIR = 'timetables'
-OUT_FILE = 'timetable_data.csv'
+OUT_FILE = 'schedule.csv'
 
 
 def list_files_in_directory(directory_path):
+    """Перебират все PDF-файлы в папке"""
     file_paths = []
     # Получаем список всех файлов и папок в указанной папке
     for file_name in os.listdir(directory_path):
         # Строим полный путь к файлу
         file_path = os.path.join(directory_path, file_name)
-        # Проверяем, является ли это файлом
+        # Проверяем, является ли это PDF-файлом
         if os.path.isfile(file_path) and file_name.endswith('.pdf'):
             file_paths.append(file_path)
     return file_paths
 
 
+def parse_timetables(timetable_dir, out_file):
+    parser = TimetablePDFParser()  # Устанавливаем парсер
+    files = list_files_in_directory(timetable_dir)  # Находим все PDF-файлы
+    df = parser.parse_all_pdfs(filepaths=files)  # Парсим их в pandas.DataFrame
+    df.to_csv(out_file, index=False)  # Экспортируем в CSV
+
+
 if __name__ == '__main__':
-    parser = TimetablePDFParser()
-    files = list_files_in_directory(TIMETABLE_DIR)
-    df = parser.parse_all_pdfs(filepaths=files)
-    df.to_csv(OUT_FILE, index=False)
+    parse_timetables(TIMETABLE_DIR, OUT_FILE)
